28445e9d9cc48749468acc981f05dce94b5a7bb2
Lucas Henrique <lucas.henrique@nstech.com.br>
2023-11-09 19:02:13 -0300

    chore: init geocoder on index file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from 'react-native';
+import Geocoder from 'react-native-geocoding';
 import App from './src/App.tsx';
 import { name as appName } from './app.json';
+
+Geocoder.init('AIzaSyDTPrB_f-LzlJ_ULhqFIX1IWd_4l4AP1YI');
 
 AppRegistry.registerComponent(appName, () => App);
