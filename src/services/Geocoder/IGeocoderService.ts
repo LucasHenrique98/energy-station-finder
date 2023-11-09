70bd4250cc9cb314f35a156d00c4574b92077a4d
Lucas Henrique <lucas.henrique@nstech.com.br>
2023-11-09 19:10:19 -0300

    feat: creates a interface for Geocoder service

---
+++ src/services/Geocoder/IGeocoderService.ts
@@ -0,0 +1,16 @@
+export type GeoLocation = {
+  latitude: number;
+  longitude: number;
+};
+
+export type GeoLocationAddressReturn = {
+  address: string;
+  number: number;
+  zipCode: string;
+  city: string;
+  state: string;
+};
+
+export interface IGeocoderService {
+  searchByGeoLocation(coords: GeoLocation): GeoLocationAddressReturn;
+}
