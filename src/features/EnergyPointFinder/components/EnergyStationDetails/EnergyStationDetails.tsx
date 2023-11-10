674b990726b7b6530f80c53781085b440bd7268b
LucasHenrique98 <henrique.lucas30@gmail.com>
2023-11-10 10:43:00 -0300

    feat: creates an EnergyStationDetails component

---
+++ src/features/EnergyPointFinder/components/EnergyStationDetails/EnergyStationDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+import { styles } from './stylesheet';
+
+export default function EnergyStationDetails() {
+  return (
+    <View>
+      <Text>EnergyStationDetails</Text>
+    </View>
+  );
+}
